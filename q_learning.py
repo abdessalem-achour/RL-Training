7e1cb3ec4e8032f616e0621a578b6be5b12f3ed0
Abdessalem achour <aabdessalemachour@hotmail.com>
2021-02-19 02:05:52 +0100

    plotting reward evolution by episodes

--- q_learning.py
+++ q_learning.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 # existing environment
 env = gym.make("MountainCar-v0") 
@@ -10,8 +11,8 @@ state = env.reset()
 # Q_fuction parameters
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95 
-EPISODES = 25000
-SHOW_EVERY = 2000
+EPISODES = 2000
+SHOW_EVERY = 500
 
 # Other parameters
 epsilon = 0.5 # exploration rate
@@ -29,7 +30,7 @@ print(env.observation_space.low)'''
 # Space descretization
 DISCRETE_OS_SIZE = [20]*len(env.observation_space.high) # definite number * number of space observations
 discret_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
-print("discret_os_win_size = ", discret_os_win_size)
+#print("discret_os_win_size = ", discret_os_win_size)
 
 def get_descret_state(state):
     descrete_state = (state - env.observation_space.low)/ discret_os_win_size
@@ -41,9 +42,12 @@ q_table = np.random.uniform(low=-2,high=0,size=(DISCRETE_OS_SIZE + [env.action_s
 #print(q_table)
 #print(q_table[descrete_state])
 
+ep_rewards =[]
+aggr_ep_rewards = {'ep': [], 'avg':[], 'min': [], 'max':[]}
 
 # Q_learning algorithm
 for episode in range(EPISODES):
+    episode_reward = 0 
     if episode % SHOW_EVERY == 0:
         print(episode)
         render = True
@@ -61,6 +65,7 @@ for episode in range(EPISODES):
             action = np.random.randint(0,env.action_space.n)
 
         new_state,reward,done,_= env.step(action)
+        episode_reward += reward
         new_descrete_state = get_descret_state(new_state)
         if render :
             env.render()
@@ -76,7 +81,26 @@ for episode in range(EPISODES):
             
         descrete_state = new_descrete_state     # update the descrete_state
     
+    
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING :
         epsilon -= epsilon_decay_value
+
+    ep_rewards.append(episode_reward)
+
+    if episode % SHOW_EVERY == 0 :
+        np.save(f"q_tables/{episode}-qtable.npy",q_table)
+        average_reward = sum(ep_rewards[-SHOW_EVERY:]) / len(ep_rewards[-SHOW_EVERY:])
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
+        aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+        print(f"Episode: {episode} , avg: {average_reward} , max: {max(ep_rewards[-SHOW_EVERY:])} , min: {min(ep_rewards[-SHOW_EVERY:])}")
         
 env.close()
+
+# Plot reward evolution by episodes
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="avg")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max")
+plt.legend(loc=2)
+plt.show()
